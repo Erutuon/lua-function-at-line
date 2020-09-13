c950022650593861a5cef332684e374fbe27b361
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-09-13 14:39:46 -0500

    example to print function name assigned to each line

--- examples/parse.rs
+++ examples/parse.rs
@@ -1,31 +1,20 @@
+use lua_function_at_line::{FunctionFinder, FunctionNameLine};
 use full_moon::parse;
-use lua_function_at_line::gather_function_line_spans;
 
 fn main() {
-    // dbg!(parse(r#"local _ = -function()
-
-    // end
-    
-    // _ = #function()
-
-    // end"#).unwrap());
-    let ast = parse(r#"local _ = call {
-        function()
-            do_something(function()
-                local _ = inside "function call"
-            end)
-        end,
-        identifier = function()
-        end,
-        ["string"] = function()
-        end,
+    let file = std::env::args_os().skip(1).next().expect("supply file name");
+    let code = std::fs::read_to_string(&file).expect("failed to read file");
+    let mut finder = FunctionFinder::new();
+    let functions = finder.get_functions(code.clone()).unwrap_or_else(|| {
+        // Show error message ignored by FunctionFinder.
+        parse(&code).unwrap();
+        std::process::exit(1);
+    }).to_owned();
+    let max_function_name = functions.iter().map(|function| function.name.len()).max();
+    for (i, line) in code.lines().enumerate() {
+        println!("{: <5}{: >width$}  {}", i, finder.function_from_line(&code, i).unwrap_or("<chunk>"), line, width = max_function_name.unwrap_or(0));
     }
-    
-    result = use_function{function()
-    end}
-    
-    use_function{function()
-    end}"#).unwrap();
-    let mut functions = vec![];
-    gather_function_line_spans(ast.nodes(), &mut functions).unwrap();
+    for FunctionNameLine { start, end, name } in functions {
+        println!("{: >width$} {:<3}..{:<3}", name, start, end, width = max_function_name.unwrap_or(0))
+    }
 }
