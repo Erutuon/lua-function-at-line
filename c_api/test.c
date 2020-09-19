44e0ed3dc84be122ac41394fc107d7390e560f86
Erutuon <5840197+Erutuon@users.noreply.github.com>
2020-09-19 04:27:26 -0500

    line up line numbers and function names in demo C program

--- c_api/test.c
+++ c_api/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <limits.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "lua_function_at_line.h"
 
@@ -24,54 +24,116 @@ char * read_file(const char * const path, size_t * len) {
     return buffer;
 }
 
+size_t count_lines(const char * str, size_t len) {
+    size_t lines = 1;
+    const char * end = str + len;
+    const char * line_break = str;
+    while (line_break < end && (line_break = memchr(line_break, '\n', end - line_break))) ++lines, ++line_break;
+    return lines;
+}
+
+typedef struct string_ref {
+    const char * ptr;
+    size_t len;
+} string_ref;
+
+string_ref * get_lines(const char * str, size_t len, size_t * out_line_count) {
+    size_t line_count = count_lines(str, len);
+    string_ref * const lines = malloc(line_count * sizeof (string_ref));
+    if (lines == NULL) exit(1);
+    if (out_line_count != NULL) *out_line_count = line_count;
+    const char * end = str + len;
+    const char * line_start = str;
+    string_ref * cur_line = lines;
+    while (line_start != NULL && line_start < end && cur_line < lines + line_count) {
+        const char * next_line_break = memchr(line_start, '\n', end - line_start);
+        size_t line_len = (next_line_break != NULL) ? next_line_break - line_start : end - line_start;
+        // Strip CR in CRLF.
+        if (next_line_break != NULL && next_line_break - 1 >= str && *(next_line_break - 1) == '\r') {
+            --line_len;
+        }
+        cur_line->ptr = line_start;
+        cur_line->len = line_len;
+        if (next_line_break == NULL) break;
+        line_start = next_line_break + 1;
+        ++cur_line;
+    }
+    return lines;
+}
+
+void get_function_names(Module * module, string_ref * const names, size_t len) {
+    string_ref * cur_name = names;
+    // This assumes that lines in full_moon are zero-based.
+    size_t line = 0;
+    while (line < len) {
+        size_t name_len = -1;
+        const char * name = lua_module_function_lines_get(module, line, &name_len);
+        cur_name->ptr = name;
+        cur_name->len = name_len;
+        ++cur_name, ++line;
+    }
+}
+
+void show_lines_with_function_names(const char * lua_code, size_t lua_code_len) {
+    Module * module = lua_module_function_lines_new(lua_code);
+    if (!module) {
+        printf("failed to parse Lua code:\n%s\n", lua_code);
+        return;
+    }
+    size_t line_count = 0;
+    string_ref * const lines = get_lines(lua_code, lua_code_len, &line_count);
+    string_ref * const function_names = malloc(line_count * sizeof (string_ref));
+    get_function_names(module, function_names, line_count);
+    if (function_names == NULL) goto free_all;
+
+    // Get longest function name for `printf` format string.
+    size_t max_function_name_len = 0;
+    for (const string_ref * name = function_names; name < function_names + line_count; ++name) {
+        if (name->len != (size_t) -1) {
+            max_function_name_len = (name->len > max_function_name_len) ? name->len : max_function_name_len;
+        }
+    }
+
+    // Get largest number of decimal digits in line count for `printf` format string.
+    size_t line_number_len = (size_t) log10((double) line_count) + 1;
+
+    size_t line_number = 1;
+    const string_ref * cur_line = lines;
+    for (string_ref * name = function_names; name < function_names + line_count; ++line_number, ++cur_line, ++name) {
+        const char * function_name = name->ptr;
+        size_t function_name_len = name->len;
+        if (function_name == NULL) {
+            function_name = "<unknown>";
+            function_name_len = sizeof "<unknown>" - 1;
+        }
+        // Assume cur_line-> len is less than or equal to INT_MAX.
+        printf(
+            "%*zu %*s%.*s %.*s\n",
+            (int) line_number_len, line_number,
+            (int) (max_function_name_len - function_name_len), "",
+            (int) function_name_len, function_name,
+            (int) cur_line->len, cur_line->ptr
+        );
+    }
+    free_all:
+    lua_module_function_lines_free(module);
+    free(function_names);
+    free(lines);
+}
+
 int main(int argc, char * * argv) {
     if (argc < 2) {
         fprintf(stderr, "expected Lua file name\n");
         return 1;
     }
-    const char * lua_path = argv[1];
-    size_t len = 0;
-    char * lua_code = read_file(lua_path, &len);
+    const char * const lua_path = argv[1];
+    size_t lua_code_len = 0;
+    char * const lua_code = read_file(lua_path, &lua_code_len);
     if (!lua_code) {
         printf("failed to read file\n");
         return 1;
     }
-    Module * module = lua_module_function_lines_new(lua_code);
-    if (!module) {
-        printf("failed to parse Lua code:\n%s\n", lua_code);
-        goto free_code;
-    }
-    size_t function_len = -1;
-    size_t line = 1;
-    const char * lua_code_end = lua_code + len;
-    // Continue incrementing `line` while `memchr` finds a newline.
-    const char * line_start = lua_code;
-    while (line_start != NULL && line_start < lua_code_end) {
-        const char * function = lua_module_function_lines_get(module, line, &function_len);
-        const char * next_line_break = memchr(line_start, '\n', lua_code_end - line_start);
-        size_t line_len = (next_line_break != NULL) ? next_line_break - line_start : lua_code_end - line_start;
-        if (function_len != -1) {
-            if (function_len > (size_t) INT_MAX) {
-                printf("%zu: %.*s ...", line, INT_MAX, function);
-            } else {
-                printf("%zu: %.*s", line, (int) function_len, function);
-            }
-        } else {
-            printf("%zu: <unknown>", line);
-        }
-        if (line_len > 0) {
-            if (line_len > (size_t) INT_MAX) {
-                printf("%.*s ...", INT_MAX, line_start);
-            } else {
-                printf("%.*s", (int) line_len, line_start);
-            }
-        }
-        printf("\n");
-        if (next_line_break == NULL) break;
-        line_start = next_line_break + 1, ++line;
-    }
-    lua_module_function_lines_free(module);
-    free_code:
+    show_lines_with_function_names(lua_code, lua_code_len);
     free(lua_code);
     return 0;
 }
